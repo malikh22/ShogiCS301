95b1212a8f4169a45cfb3303e78f5b9e99bc5608
malikh22 <malik22@up.edu>
2020-04-23 18:10:57 -0700

    potential fix for friendly fire??

--- app/src/main/java/com/example/shogics301/ShogiLocalGame.java
+++ app/src/main/java/com/example/shogics301/ShogiLocalGame.java
@@ -91,7 +91,7 @@ public class ShogiLocalGame extends LocalGame {
                             newBoard[row][col].setPlayer(1);
                             gameState.capturep1(newBoard[row][col]);
                         }
-                        newBoard[oldRow][oldCol] = null;
+                        newBoard[row][col] = newBoard[oldRow][oldCol];
                     }
 
                     //Create piece in new spot
